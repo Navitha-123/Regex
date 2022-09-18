4adf966fd809d19a26f2ab7670eb80eaf7acd396
navitha <navithareddy112@gmail.com>
2022-09-18 20:56:49 +0530

    [navitha] UC1RegexFirstName

---
+++ RegexUserRegistration/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexUserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("choose option 1.firstname");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    UC1RegexFirstName.ValidFirstName();
+                    break;
+                default:
+                    Console.WriteLine("enter wrong choice");
+                    break;
+            }
+        }
+    }
+}
---
+++ RegexUserRegistration/UC1RegexFirstName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUserRegistration
+{
+    internal class UC1RegexFirstName
+    {
+        public static void ValidFirstName()
+        {
+            Console.WriteLine("Enter first name");
+            string firstname = Console.ReadLine();
+            string pattern = "^[A-Z],{3}";
+            if(Regex.IsMatch(firstname, pattern))
+            {
+                Console.WriteLine("valid first name");
+
+            }
+            else
+            {
+                Console.WriteLine("In valid first name");
+            }
+        }
+    }
+}
